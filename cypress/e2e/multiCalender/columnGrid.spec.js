590bdbb99c0e61eab34e5423b9dbe0196758fa21
meiyalagan <meiyalagan@klenty.com>
2026-03-13 11:36:26 +0530

    misc

--- cypress/e2e/multiCalender/columnGrid.spec.js
+++ cypress/e2e/multiCalender/columnGrid.spec.js
@@ -10,37 +10,19 @@ describe("Multicalender DSO feature", () => {
     });
 
     it('selects 28 and 30 cells using drag', () => {
+      const listingName = "192 Seasonal Property"
 
-        // ensure listing row is visible
-        searchPage.searchAndValidate("192 Seasonal Property")
-      
-        cy.contains('tr', '192 Seasonal Property', { timeout: 10000 })
-          .should('be.visible')
-          .within(() => {
-      
-            // wait until cells load
-            cy.get('[qa-id*="price-tooltip--"]', { timeout: 10000 })
-              .should('have.length.greaterThan', 5)
-      
-            // get start cell again (fresh query)
-            // start drag
-            cy.get('[qa-id="price-tooltip--VRMREALTY___108-9"]')
-            .scrollIntoView()
-            .should('be.visible')
-              .trigger('mousedown', { button: 0, force: true })
-      
-            // get end cell separately (fresh DOM query)
-            cy.get('[qa-id="price-tooltip--VRMREALTY___108-10"]')
-              .scrollIntoView()
-              .should('be.visible')
-              .trigger('mousemove', { button: 0, force: true })
-      
-            // release mouse
-            cy.get('[qa-id="price-tooltip--VRMREALTY___108-10"]')
-              .trigger('mouseup', { force: true })
-      
-          })
+      // ensure listing row is visible
+      searchPage.searchAndValidate(listingName)
+
+      // wait until cells load for this listing
+      cy.findListingRow(listingName).within(() => {
+        cy.get('[qa-id*="price-tooltip--"]', { timeout: 10000 })
+          .should('have.length.greaterThan', 5)
       })
-    
+
+      // use shared helper to drag-select cells
+      cy.dragSelectPricingCells(listingName, 9, 10)
+    })
   })
 })
